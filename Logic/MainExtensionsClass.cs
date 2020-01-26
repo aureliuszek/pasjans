997a594c4aef0069a7c6120794b3acd98e86b4ba
Paweł Wójsik <pablo8899@interia.pl>
2020-01-26 10:03:40 +0100

    Poprawki

---
+++ Logic/MainExtensionsClass.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Image = System.Windows.Controls.Image;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using System.Reflection;
+using Brushes = System.Windows.Media.Brushes;
+
+namespace Logic
+{
+    public enum StackNames
+    {
+        Reserve,
+        Base1,
+        Base2,
+        Base3,
+        Base4,
+        Base5,
+        Base6,
+        Base7,
+        Base8,
+        Main1,
+        Main2,
+        Main3,
+        Main4,
+        Main5,
+        Main6,
+        Main7,
+        Main8,
+        Main9,
+        Main10
+    }
+
+    public static class Extensions
+    {
+        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+
+
+        public static void SetUpCardsToStacks(this List<Stack> stacks, List<Card> cards)
+        {
+            stacks.GetElementByName(StackNames.Reserve).AddCardsToStackFromListOfNumer(cards, 50);
+
+            stacks.GetElementByName(StackNames.Main1).AddCardsToStackFromListOfNumer(cards, 6);
+            stacks.GetElementByName(StackNames.Main2).AddCardsToStackFromListOfNumer(cards, 6);
+            stacks.GetElementByName(StackNames.Main3).AddCardsToStackFromListOfNumer(cards, 6);
+            stacks.GetElementByName(StackNames.Main4).AddCardsToStackFromListOfNumer(cards, 6);
+            stacks.GetElementByName(StackNames.Main5).AddCardsToStackFromListOfNumer(cards, 5);
+            stacks.GetElementByName(StackNames.Main6).AddCardsToStackFromListOfNumer(cards, 5);
+            stacks.GetElementByName(StackNames.Main7).AddCardsToStackFromListOfNumer(cards, 5);
+            stacks.GetElementByName(StackNames.Main8).AddCardsToStackFromListOfNumer(cards, 5);
+            stacks.GetElementByName(StackNames.Main9).AddCardsToStackFromListOfNumer(cards, 5);
+            stacks.GetElementByName(StackNames.Main10).AddCardsToStackFromListOfNumer(cards, 5);
+        }
+
+        public static void ClearStacks(this List<Stack> stacks)
+        {
+            LogicResources.PickedCards.Clear();
+            foreach (Stack s in stacks)
+            {
+               foreach(Card c in s.Cards)
+                {
+                    c.Clear();
+                }
+                s.Cards.Clear();
+            }
+        }
+        public static int GetElementPosition(this List<Card> cards,Card c)
+        {
+            return cards.FindIndex(x => x == c) + 1;
+        }
+
+        public static int GetElementIndex(this List<Card> cards, Card c)
+        {
+            return cards.FindIndex(x => x == c);
+        }
+
+        public static Stack GetElementByName(this List<Stack> stacks, StackNames name)
+        {
+            return stacks.Where(x => x.Name == name.ToString()).First();
+        }
+
+        public static void DeleteCardsFromStack(this List<Card> cards)
+        {
+            foreach (Card c in cards)
+            {
+                LogicResources.Main.Children.Remove(c.Image);
+                c.Stack.Cards.Remove(c);
+            }
+        }
+
+        public static void AddCardsToStack(this List<Card> cards,Stack s)
+        {
+            foreach (Card c in cards)
+            {
+                s.AddCardToStack(c.Copy(), true);
+            }
+        }
+    }
+}
---
+++ Logic/MainStackClass.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using Brushes = System.Windows.Media.Brushes;
+namespace Logic
+{
+    public enum StackType
+    {
+        Reserve,
+        Base,
+        Main
+    }
+    public class Stack
+    {
+        public string Name { get; set; }
+        public StackType Type { get; set; }
+        public List<Card> Cards = new List<Card>();
+        public Border Border;
+        public Canvas Canvas { get; set; }
+        public Thickness Location;
+        public Stack(string name, StackType type, Border border)
+        {
+            Name = name;
+            Type = type;
+            Border = border;
+            Location = border.Margin;
+            if (type == StackType.Main)
+            {
+                Canvas = CreateStackCanvas();
+                LogicResources.Main.Children.Add(Canvas);
+                Canvas.MouseEnter += Canvas_MouseEnter;
+            }
+        }
+
+        public void Canvas_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Functions.ChangeAllCanvasOfMainStacksZindex(-1);
+
+            if (!LogicResources.Picked && LogicResources.PickedCards.Count() != 0)
+            {
+                Stack s = LogicResources.PickedCards.First().Stack;
+                LogicResources.PickedCards.DeleteCardsFromStack();
+                if (Cards.Count() != 0 && LogicResources.PickedCards.First().Priority != Cards.Last().Priority - 1 || s == this)
+                {
+                    LogicResources.PickedCards.AddCardsToStack(s);
+                } else
+                {
+                    if (s.Cards.Count() != 0)
+                        s.Cards.Last().Visible = true;
+                    LogicResources.PickedCards.AddCardsToStack(this);
+                }
+
+
+                LogicResources.PickedCards.Clear();
+            }
+        }
+
+        public void AddCardsFromReserve()
+        {
+            List<Card> listOfCards = new List<Card>();
+
+            int i = 9;
+            foreach (Card c in Cards)
+            {
+                LogicResources.Stacks[i].AddCardToStack(c.Copy(), true);
+                listOfCards.Add(c);
+                i++;
+                if (i == 19)
+                    break;
+            }
+            listOfCards.DeleteCardsFromStack();
+        }
+
+        private void CheckIfFullCardSort()
+        {
+            List<Card> cards = new List<Card>();
+            foreach(Card c in Cards)
+            {
+                if (c.Name == "K")
+                {
+                    cards.Clear();
+                    cards.Add(c);
+                }
+                else if (c.Visible == true && (c.GetNextCardPriorityAndCheck() && c.GetPreviousCardPriorityAndCheck()))
+                {
+                    cards.Add(c);
+                } else
+                {
+                    cards.Clear();
+                }
+            }
+
+            if(cards.Count() == 13 && cards.All(x => x.Type == cards.First().Type))
+            {
+                Stack s = Functions.GetFreeBaseStack();
+
+                foreach (Card c in cards)
+                {
+                    s.AddCardToStack(c.Copy(), true);
+                }
+                cards.DeleteCardsFromStack();
+
+                if (Cards.Count() != 0)
+                    Cards.Last().Visible = true;
+
+                Functions.CheckIfWin();
+            }
+
+
+        }
+
+        public Canvas CreateStackCanvas()
+        {
+
+            return new Canvas
+            {
+                Margin = Border.Margin,
+                Height = LogicResources.MainWindow.Height - Border.Margin.Top,
+                Width = LogicResources.CardWidth,
+                Background = Brushes.Transparent
+             };
+        }
+        public bool CheckIfCanMove(Card c)
+        {
+            if (!c.Visible)
+                return false;
+
+            while(GetNextCardInStack(c) != null)
+            {
+                if (c.Priority != GetNextCardInStack(c).Priority + 1 || c.Type != GetNextCardInStack(c).Type)
+                    return false;
+                return CheckIfCanMove(GetNextCardInStack(c));
+            }
+            return true;
+        }
+
+        private Card GetNextCardInStack(Card c)
+        {
+            try
+            {
+                return Cards[Cards.GetElementIndex(c) + 1];
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+        private Card GetPreviousCardInStack(Card c)
+        {
+            try
+            {
+                return Cards[Cards.GetElementIndex(c) - 1];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<Card> GetAllCardsAfterCard(Card c)
+        {
+            List<Card> cardList = new List<Card>();
+            for (int i = Cards.GetElementPosition(c); i < Cards.Count(); i++)
+            {
+                cardList.Add(Cards[i]);
+            }
+            return cardList;
+        }
+
+        public List<Card> GetAllCardsFromCardIndexToEndOfTheStack(Card c)
+        {
+            List<Card> cardList = new List<Card>();
+            for (int i = Cards.GetElementIndex(c); i < Cards.Count(); i++)
+            {
+                cardList.Add(Cards[i]);
+            }
+            return cardList;
+        }
+
+        public void AddCardsToStackFromListOfNumer(List<Card> list, int numbers)
+        {
+            int i = 0;
+            Random random = new Random();
+            while (i != numbers)
+            {
+                i++;
+                int r = random.Next(list.Count);
+                AddCardToStack(list[r], i == numbers && Type != StackType.Reserve);
+                list.RemoveAt(r);
+            }
+        }
+
+        public void AddCardToStack(Card card, bool visible)
+        {
+            Cards.Add(card);
+            card.Stack = this;
+            card.Zindex = Cards.Count();
+            card.Visible = visible;
+            card.Show();
+            if(Type == StackType.Main)
+                CheckIfFullCardSort();
+        }
+        
+        public void Clear()
+        {
+        }
+    }
+}
